e8bca8eb98e631fb8008f966a6af94f0309cf522
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-05-05 14:15:35 +0300

    Structure of the website

---
+++ includes/HomeData.php
@@ -0,0 +1,232 @@
+<div class="container">
+    <div class="print weekend">
+        <div class="header">
+            <div class="col">
+                <p>
+                    <strong>Classe: </strong>
+                    <span>P6 </span>
+                </p>
+                <p>
+                    <strong>Titulaire: </strong>
+                    <span>Bisimwa </span>
+                </p>
+                <p>
+                    <strong>Semaine: </strong>
+                    <span>24 au 27 avril 2023 </span>
+                </p>
+                <p>
+                    <strong>Fait a Gisenyi le </strong>
+                    <span>23-04-2023 </span>
+                </p>
+            </div>
+            <div class="col flex align-items-center">
+                <div>
+                    <h2>Isoko - La Source</h2>
+                    <small>Annee scolaire: 2022 - 2023</small>
+                </div>
+                <div class="logoSchool">
+                    <img src="" alt="Logo" />
+                </div>
+            </div>
+        </div>
+        <!-- <div class="studentList">
+            <div class="wrapper">
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Tona.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Prodige.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Owen.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+                <div class="student">
+                    <img src="./public/img/Prodige.jpg" alt="Student Image" />
+                    <span>Student name</span>
+                </div>
+            </div>
+        </div> -->
+
+        <div class="rows">
+            <div class="col">
+                <h3>Lundi</h3>
+                <table>
+                    <thead>
+                        <th>Heures</th>
+                        <th>Cours</th>
+                        <th>Lecons</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="info">
+                    <div class="icon">
+                        <img src="./public/img/Tona.jpg" alt="Icon" />
+                    </div>
+                    <p>
+                        <strong>communiquer: </strong> Les eleves doivent avoir leur
+                        journal chaque jour a l'ecole. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Harum, nemo. Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit. Ad fugit iste excepturi culpa
+                        vel laudantium incidunt beatae cupiditate, accusamus adipisci?
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <h3>Mardi</h3>
+                <table>
+                    <thead>
+                        <th>Heures</th>
+                        <th>Cours</th>
+                        <th>Lecons</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="info">
+                    <div class="icon">
+                        <img src="./public/img/Tona.jpg" alt="Icon" />
+                    </div>
+                    <p>
+                        <strong>communiquer: </strong> Les eleves doivent avoir leur
+                        journal chaque jour a l'ecole. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Harum, nemo. Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit. Ad fugit iste excepturi culpa
+                        vel laudantium incidunt beatae cupiditate, accusamus adipisci?
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <h3>Mardi</h3>
+                <table>
+                    <thead>
+                        <th>Heures</th>
+                        <th>Cours</th>
+                        <th>Lecons</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <small>9h-9h50</small>
+                            </td>
+                            <td><small> Francais </small></td>
+                            <td>Conjugaison: Les verbes du deuxieme groupe</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="info">
+                    <div class="icon">
+                        <img src="./public/img/Tona.jpg" alt="Icon" />
+                    </div>
+                    <p>
+                        <strong>communiquer: </strong> Les eleves doivent avoir leur
+                        journal chaque jour a l'ecole. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Harum, nemo. Lorem ipsum dolor sit
+                        amet consectetur adipisicing elit. Ad fugit iste excepturi culpa
+                        vel laudantium incidunt beatae cupiditate, accusamus adipisci?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -16,42 +16,18 @@
     <?php require_once("./includes/navbar.php")?>
 
     <div class="tab-content">
-        <div class="tab-pane container active" id="home">...</div>
-        <div class="tab-pane container fade" id="menu1">...</div>
-        <div class="tab-pane container fade" id="menu2">...</div>
-    </div>
-
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card card-body shadow-sm">
-                    <h3>Journal du <?php echo date('D, d:m:Y');?></h3>
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <label for="heure">Heures</label>
-                            <input type="text" name="heures" id="heure" placeholder="Heure..." class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="cours">cours</label>
-                            <input type="text" name="cours" id="cours" placeholder="Cours..." class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="lecon">Lecon</label>
-                            <textarea name="lecon" id="lecon" placeholder="Lecon" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="Communiquer">Communiquer</label>
-                            <textarea name="Communiquer" id="Communiquer" placeholder="Communiquer"
-                                class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-success btn-md">Enregistrer</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div class="tab-pane container active" id="home">
+            <?php require_once("./includes/HomeData.php");?>
+        </div>
+        <div class="tab-pane container fade" id="menu1">
+            <?php require_once("./includes/registerJournal.php");?>
+        </div>
+        <div class="tab-pane container fade" id="menu2">
+            table des eleves.
         </div>
     </div>
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
